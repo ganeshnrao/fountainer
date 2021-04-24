189f7f9d66f02ba627ac67fe81fb1aeac1174618
Ganesh Rao <ganeshnrao@me.com>
2021-04-23 20:14:51 -0700

    Implement regex for synopsis

--- parser/regex.js
+++ parser/regex.js
@@ -5,5 +5,6 @@ module.exports = {
   parenthetical: /^\s*(?<paren>\(.*\))/i,
   sceneHeading: /(^(?<scene>((INT\/EXT|I\/E|INT\.\/EXT|INT|EXT|EST)\.){1}\s*.*)$)|(^\.(?<pScene>[a-z]+.*)$)/i,
   section: /^(?<depth>#+)\s*(?<section>.+)/i,
+  synopsis: /^=\s*(?<synopsis>.+)/i,
   transition: /^\s*(?<transition>(FADE [^a-z]+:)|([^a-z]+ TO:)\s*)|(>\s*(?<pTransition>[^\<]+))$/
 }
