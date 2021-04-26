56a5341def6ab2ab196db453fa2fab77e12b404d
Ganesh Rao <ganeshnrao@me.com>
2021-04-26 10:53:19 -0700

    Minify output HTML

--- parser/toHtml.js
+++ parser/toHtml.js
@@ -3,6 +3,7 @@ const { template } = require('lodash')
 const sass = require('sass')
 const path = require('path')
 const logger = require('./logger')
+const { minify } = require('html-minifier')
 
 module.exports = function (
   parsed,
@@ -12,11 +13,18 @@ module.exports = function (
   } = {}
 ) {
   logger.verbose(`Rendering SCSS file ${scssPath}`)
-  const result = sass.renderSync({ file: scssPath })
+  const result = sass.renderSync({ file: scssPath, outputStyle: 'compressed' })
   const css = result.css.toString()
   logger.verbose(`Reading template ${templatePath}`)
   const templateString = fs.readFileSync(templatePath)
   logger.verbose('Compiling HTML')
   const compileHtml = template(templateString)
-  return compileHtml({ ...parsed, css })
+  const html = compileHtml({ ...parsed, css })
+  logger.verbose('Minifying HTML')
+  return minify(html, {
+    removeComments: true,
+    removeEmptyAttributes: true,
+    html5: true,
+    collapseWhitespace: true
+  })
 }
