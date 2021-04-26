29fb97ad2621565fff690cebfea1b9aa774d4b32
Ganesh Rao <ganeshnrao@me.com>
2021-04-26 00:11:26 -0700

    Remove extra sample file

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const fs = require('fs-extra')
 const path = require('path')
 const { parse, toHtml } = require('./parser')
-const yargs = require('yargs')
 
-const { sample } = yargs.options({
-  sample: {
-    alias: 's',
-    choices: ['sample.theBigFish.fountain', 'sample.theSearcher.fountain'],
-    description: 'specify the fountain file to render',
-    default: 'sample.theBigFish.fountain'
-  }
-}).argv
-
-const samplePath = path.resolve(__dirname, sample)
+const samplePath = path.resolve(__dirname, 'sample.theBigFish.fountain')
 const htmlPath = path.resolve(__dirname, 'dist/index.html')
 const jsonPath = path.resolve(__dirname, 'dist/parsed.json')
 
 async function main() {
-  console.log(`Building ${sample}`)
   const outputDir = path.dirname(htmlPath)
   const parsed = await parse({
     inputFile: samplePath,
